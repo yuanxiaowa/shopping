e9b991477f7d26b8240a9dc70f51bff213a040be
欧顶银 <oudingyin@oudingyindeMacBook-Pro.local>
2019-10-04 16:54:05 +0800

    优化延迟时间

--- utils/tools.ts
+++ utils/tools.ts
@@ -320,7 +320,7 @@ export async function sysPlatformTime(platform: string) {
     (dt > 0 ? "慢了" : "快了") + Math.abs(dt) + "ms"
   );
   console.log(platform + "单程时间", rtl + "ms");
-  DT[platform] = dt + (platform === "taobao" ? Math.max(0, rtl - 50) : rtl);
+  DT[platform] = dt + (platform === "taobao" ? Math.max(0, rtl - 30) : rtl);
 }
 
 const getDelayTime = (() => {
@@ -333,13 +333,13 @@ const getDelayTime = (() => {
     if (toTime.format("mm:ss") !== "00:00") {
       return toTime.diff(moment()) - DT[platform];
     }
-    let bt = 1000 * 60 * 10
+    let bt = 1000 * 60 * 10;
     let p = new Promise<number>(resolve => {
       setTimeout(async () => {
         await sysPlatformTime(platform);
         setTimeout(() => {
-          delete map[t]
-        }, bt)
+          delete map[t];
+        }, bt + 5000);
         resolve(toTime.diff(moment()) - DT[platform]);
       }, toTime.diff(moment()) - DT[platform] - bt);
     });

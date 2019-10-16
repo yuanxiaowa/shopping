b53125b5a564830a83b7aebcb159d6099a841e3a
欧顶银 <oudingyin@promote.cache-dns.local>
2019-10-16 23:44:12 +0800

    优化下单

--- utils/page.ts
+++ utils/page.ts
@@ -16,7 +16,7 @@ var defaultPage: Page;
 
 export async function bootstrapBrowser() {
   var revisionInfo = await resolver();
-  var dataDir = process.cwd() + "/.data/data-dir4";
+  var dataDir = process.cwd() + "/.data/data-dir";
   browser = await puppeteer.launch({
     headless: false,
     userDataDir: dataDir,
